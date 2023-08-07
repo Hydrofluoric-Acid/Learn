8b7b2fb2308deffef47d151fd061b9c337a01507
vcjt <3535664788@qq.com>
2023-08-07 18:12:18 +0800

    ATM system

--- Java/java_advanced/ATMsystem.java
+++ Java/java_advanced/ATMsystem.java
@@ -1,10 +1,109 @@
+import java.util.ArrayList;
+import java.util.Scanner;
+
 public class ATMsystem {
 	public static void main(String[] args) {
-		
+
 	}
-	
-}
-class User{
-	
+
 }
 
+class User {
+	protected String name, gender, password, account;
+	Double credit_limit;
+	public User() {
+
+	}
+	public String get_name(){
+		return name;
+	}
+	public String get_account(){
+		return account;
+	}
+}
+
+class UserSystem {
+	private ArrayList<User> users = new ArrayList<>();
+	private User user;
+	Scanner sc = new Scanner(System.in);
+
+	public UserSystem() {
+
+	}
+
+	public void openAccount() {
+		System.out.println("请输入您的账户名称：");
+		user.name = sc.nextLine();
+		System.out.println("请输入您的性别：");
+		user.gender = sc.nextLine();
+		System.out.println("请输入您的账户密码：");
+		user.password = sc.nextLine();
+		System.out.println("请输入您的确认密码：");
+		user.password = sc.nextLine();
+		System.out.println("请输入您的取现额度：");
+		user.credit_limit = sc.nextDouble();
+		System.out.println("恭喜您，" + user.name + "开户成功，您的卡号是：" + user.account);
+		users.add(user);
+
+	}
+
+	public void loginSystem() {
+		System.out.println("==系统登录==");
+		System.out.println("");
+		String preAccount = sc.nextLine();
+		System.out.println("");
+		String prePassword = sc.nextLine();
+		for (User user : users) {
+			if (preAccount.equals(user.account) && prePassword.equals(user.password)) {
+				this.operateSystem();
+			}
+		}
+	}
+
+	public void operateSystem() {
+		while (true) {
+			switch (sc.nextInt()) {
+				case 1:
+
+					break;
+				case 21:
+
+					break;
+				case 3:
+
+					break;
+				case 4:
+
+					break;
+				case 5:
+
+					break;
+				case 6:
+
+					break;
+				case 7:
+
+					break;
+				default:
+					break;
+			}
+		}
+
+	}
+
+	public void start() {
+		while (true) {
+			int command = sc.nextInt();
+			switch (command) {
+				case 1:
+					this.loginSystem();
+					break;
+				case 2:
+					this.openAccount();
+					break;
+				default:
+					break;
+			}
+		}
+	}
+}
--- Java/java_advanced/ClassDemo.java
+++ Java/java_advanced/ClassDemo.java
@@ -1,4 +1,3 @@
-package Java.java_advanced;
 
 public class ClassDemo {
     public static void main(String[] args) {
