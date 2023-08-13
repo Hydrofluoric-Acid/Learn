f10070114527247920c467ab7b60966e4363ea01
vcjt <3535664788@qq.com>
2023-08-13 18:12:54 +0800

    finish gomoku game

--- Java/Gomoku.java
+++ Java/Gomoku.java
@@ -1,4 +1,4 @@
-import java.util.Arrays;
+import java.util.ArrayList;
 import java.util.Scanner;
 
 public class Gomoku {
@@ -11,45 +11,103 @@ public class Gomoku {
 
 class Play {
     private int[][] arr = new int[15][15];
-
+    private String[] index = { "1", "2", "3", "4", "5", "6", "7", "8", "9", "A", "B", "C", "D", "E", "F" };
     public void printChessBoard() {
+        StringBuilder sb = new StringBuilder();
+        sb.append("1 2 3 4 5 6 7 8 9 A B C D E F\n");
         for (int i = 0; i < arr.length; i++) {
             for (int j = 0; j < arr.length; j++) {
                 switch (arr[i][j]) {
                     case 0:
-                        System.out.print("+ ");
+                        sb.append("+ ");
                         break;
                     case 1:
-                        System.out.print("\u25CF ");
+                        sb.append("\u25CF ");
                         break;
-                    case 2:
-                        System.out.print("\u25CB ");
+                    case -1:
+                        sb.append("\u25CB ");
                         break;
                 }
             }
-            System.out.println(" ");
+            sb.append(index[i] + "\n");
         }
+        System.out.print(sb);
     }
-    public void start(){
-        Scanner sc=new Scanner(System.in);
+
+    public void start() {
+        boolean flag = true;
+        Scanner sc = new Scanner(System.in);
         while (true) {
-            String A=sc.nextLine();
-            change(A,1);
-            printChessBoard();
-            String B=sc.nextLine();
-            change(B,2);
-            printChessBoard();
-            
-            
+            String A = sc.nextLine();
+            flag = change(A, 1);
+            String B = sc.nextLine();
+            flag = change(B, -1);
         }
     }
-    public void change(String str,int flag){
-        int i=Integer.parseInt((str.split(",")[0]));
-        int j=Integer.parseInt((str.split(",")[1]));
-        if (arr[i][j]!=1 && arr[i][j]!=2) {
-            arr[i][j]=flag;
+
+    public boolean change(String str, int c) {
+        for (int i = 0; i < 6; i++) {
+            str = str.replace(index[i + 9], Integer.toString(i + 10));
         }
-        
+
+        int i = Integer.parseInt((str.split(",")[0])) - 1;
+        int j = Integer.parseInt((str.split(",")[1])) - 1;
+        if (arr[i][j] != 1 && arr[i][j] != -1) {
+            arr[i][j] = c;
+        }
+        this.printChessBoard();
+        return this.judge();
+    }
+
+    public boolean judge() {
+        return (judge_nums_horizontal()) || (judge_diagonal());
+    }
+
+    public boolean judge_diagonal() {
+        ArrayList<Integer> nums_L_diagonal = new ArrayList<>();
+        ArrayList<Integer> nums_R_diagonal = new ArrayList<>();
+        for (int i = 0; i < 10; i++) {
+            for (int j = 0; j < 5; j++) {
+                nums_L_diagonal.add(arr[i][i]);
+                nums_R_diagonal.add(arr[14 - i][i]);
+            }
+            if ((isEqual(sum(nums_L_diagonal))) || (isEqual(sum(nums_R_diagonal)))) {
+                return false;
+            }
+        }
+        return true;
+    }
+
+    public boolean judge_nums_horizontal() {
+        ArrayList<Integer> nums_horizontal = new ArrayList<>();
+        ArrayList<Integer> nums_vertical = new ArrayList<>();
+        for (int start = 0; start < 11; start++) {
+            for (int a = 0; a < 15; a++) {
+                for (int b = start; b < start + 5; b++) {
+                    nums_horizontal.add(arr[a][b]);
+                    nums_vertical.add(arr[b][a]);
+                }
+                if ((isEqual(sum(nums_vertical))) || (isEqual(sum(nums_horizontal)))) {
+                    return false;
+                }
+            }
+        }
+        return true;
+    }
+
+    public int sum(ArrayList<Integer> arr) {
+        int sum = 0;
+        for (Integer integer : arr) {
+            sum += integer;
+        }
+        return sum;
+    }
+
+    public boolean isEqual(int n) {
+        if (n == 5 || n == -5) {
+            return true;
+        } else {
+            return false;
+        }
     }
 }
-    
